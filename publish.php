143dbc8f025beed838d58b5fd301dbdbab5c82a0
Richard Sbresny <voidet@nightgen.com>
2018-12-25 20:08:29 -0800

    Tue Dec 25 20:08:29 PST 2018

---
+++ publish.php
@@ -0,0 +1,19 @@
+<?php
+
+$files = array_diff(scandir("_posts"), array('.', '..'));
+
+$md5string = "";
+foreach ($files as $file) {
+  $md5string .= md5_file("_posts/".$file);
+}
+
+$oldmd5 = file_get_contents("oldmd5");
+$newmd5 = md5($md5string);
+
+if ($oldmd5 != $newmd5) {
+  shell_exec("./deploy.sh");
+} else {
+  echo "Same";
+}
+
+file_put_contents("oldmd5", $newmd5);
